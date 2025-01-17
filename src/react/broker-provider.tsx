76a9e1a3dd2e2afd41b2b218f9c0e7b63147bd2c
andre-hctulc <andre.tho.44@gmail.com>
2025-01-17 22:52:30 +0100

    >>

--- src/react/broker-provider.tsx
+++ src/react/broker-provider.tsx
@@ -1,33 +1,70 @@
 import { createContext, FC, useContext, useMemo } from "react";
 import { Broker, BrokerConfig, BrokerInterface } from "../broker.js";
 
-const Context = createContext<BrokerContext | null>(null);
+const BrokerContext = createContext<BrokerContext | null>(null);
 
 export interface BrokerContext<I extends BrokerInterface = BrokerInterface> {
     broker: Broker<I>;
+    id: string | null;
+    superBroker: BrokerContext | null;
 }
 
-export function useBroker<I extends BrokerInterface = BrokerInterface>(): BrokerContext<I> {
-    const ctx = useContext(Context);
+/**
+ * @param brokerId Broker ID. If provided, the next broker with this ID will be returned.
+ */
+export function useBroker<I extends BrokerInterface = BrokerInterface>(brokerId?: string): BrokerContext<I> {
+    const ctx = useContext(BrokerContext);
+    const idBroker = useMemo<BrokerContext | null>(() => {
+        if (!brokerId) return null;
+
+        let currentCtx: BrokerContext | null = ctx;
+
+        while (currentCtx) {
+            if (currentCtx.id === brokerId) {
+                return currentCtx;
+            }
+            currentCtx = currentCtx.superBroker;
+        }
+
+        return null;
+    }, [ctx, brokerId]);
+
+    if (brokerId) {
+        if (!idBroker) {
+            throw new Error(`Broker with ID "${brokerId}" not found`);
+        }
+        return idBroker as any;
+    }
+
     if (!ctx) {
         throw new Error("`BrokerContext` not found");
     }
+
     return ctx as any;
 }
 
 interface BrokerProviderProps {
     children?: React.ReactNode;
     /**
-     * Broker configuration. 
-     * 
+     * Broker configuration.
+     *
      * **This is a reactive property.** If the configuration changes, the broker will be recreated.
      */
     config?: BrokerConfig;
+    /**
+     * Broker ID. If provided, the broker will be created with this ID.
+     */
+    id?: string;
 }
 
-export const BrokerProvider: FC<BrokerProviderProps> = ({ children, config }: BrokerProviderProps) => {
+export const BrokerProvider: FC<BrokerProviderProps> = ({ children, config, id }: BrokerProviderProps) => {
     const broker = useMemo(() => {
         return new Broker(config);
     }, [config]);
-    return <Context.Provider value={{ broker }}>{children}</Context.Provider>;
+    const superBrokerCtx = useContext(BrokerContext);
+    return (
+        <BrokerContext.Provider value={{ broker, superBroker: superBrokerCtx || null, id: id ?? null }}>
+            {children}
+        </BrokerContext.Provider>
+    );
 };
